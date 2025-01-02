4c16d95d7f9acaed53e06c9865cb2058a126b035
aminot01 <aminotjs24@gmail.com>
2025-01-01 19:24:18 -0500

    Added function for stay button with evaluation of who wins the game and displaying end screen accordingly

--- main.js
+++ main.js
@@ -9,8 +9,10 @@ const cardVals = [2,3,4,5,6,7,8,9,10,"J","Q","K","A"];
 // variables to save dealer and players soft and hard scores
 let dealerSoftScore = 0;
 let dealerHardScore = 0;
+let dealerHasAce = false;
 let playerSoftScore = 0;
 let playerHardScore = 0;
+let playerHasAce = false;
 
 // function that returns random card value
 function getRandomCardVal() {
@@ -31,8 +33,14 @@ function updatePlayerScores() {
             playerSoftScore += 10;
             playerHardScore += 10;
         }
+        // score needs to be tracked treating the first ace as a 1 or 11
         else if (cards[i].innerHTML == "A") {
-            playerSoftScore += 10;
+            if (!playerHasAce) {
+                playerSoftScore += 11;
+            }
+            else {
+                playerSoftScore += 1;
+            }
             playerHardScore += 1;
         }
         else {
@@ -57,7 +65,12 @@ function updateDealerScores() {
             dealerHardScore += 10;
         }
         else if (cards[i].innerHTML == "A") {
-            dealerSoftScore += 10;
+            if (!dealerHasAce) {
+                dealerSoftScore += 11;
+            }
+            else {
+                dealerSoftScore += 1;
+            }
             dealerHardScore += 1;
         }
         else {
@@ -81,7 +94,7 @@ let playerCardId = 3
 
 // function for event handler for the hit button
 function handleClickHit(event) {
-    // create new card html element
+    // create new card html element for player
     let newCard = document.createElement("section");
     let id = "playersCard" + playerCardId;
     playerCardId += 1;
@@ -93,11 +106,11 @@ function handleClickHit(event) {
     updatePlayerScores();
     // check if player busted
     if (playerHardScore > 21) {
-        document.getElementById("outcome").innerHTML = "BUST";
+        document.getElementById("outcome").innerHTML = "BUST! YOU LOSE";
         endGame();
     }
     if (playerHardScore == 21 || playerSoftScore == 21) {
-        document.getElementById("outcome").innerHTML = "BLACK JACK";
+        document.getElementById("outcome").innerHTML = "BLACK JACK! YOU WIN";
         endGame();
     }
 }
@@ -106,14 +119,64 @@ function handleClickHit(event) {
 hit.addEventListener("click", handleClickHit);
 
 //function for event handler for the stay button
+function handleClickStay(event) {
+    let dealerDraws = 5;
+    let dealerCardId = 2;
+    updateDealerScores();
+
+    while (dealerDraws != 0) {
+        // create new card html element for dealer
+        let newCard = document.createElement("section");
+        let id = "dealersCard" + dealerCardId;
+        dealerCardId += 1;
+        newCard.setAttribute("id", id);
+        newCard.setAttribute("class", "dealerCard");
+        // assign card val
+        newCard.innerHTML = getRandomCardVal();
+        document.getElementById("dealers_hand").appendChild(newCard);
+        updateDealerScores();
+        // if dealer busts game ends
+        if (dealerHardScore > 21) {
+            document.getElementById("outcome").innerHTML = "DEALER BUST! YOU WIN";
+            break;
+        }
+        // if dealer black jacks game ends
+        else if (dealerHardScore == 21 || dealerSoftScore == 21) {
+            document.getElementById("outcome").innerHTML = "DEALER BLACK JACK! YOU LOSE";
+            break;
+        }
+        // if dealer score is 17 or up game ends
+        else if ((dealerSoftScore < 21 && dealerHardScore >= 17) || (dealerSoftScore >= 17)) {
+            // if soft score isnt busted assign it to final score otherwise use hard score
+            let playerFinalScore = (playerSoftScore < 21) ? playerSoftScore : playerHardScore;
+            let dealerFinalScore = (dealerSoftScore < 21) ? dealerSoftScore : dealerHardScore;
+            // compare scores to see who wins
+            if (playerFinalScore > dealerFinalScore) {
+                document.getElementById("outcome").innerHTML = "YOU WIN";
+            }
+            else if (dealerFinalScore > playerFinalScore) {
+                document.getElementById("outcome").innerHTML = "YOU LOSE";
+            }
+            else {
+                document.getElementById("outcome").innerHTML = "TIE";
+            }
+            break;
+        }
+        dealerDraws -= 1;
+
+    }
+    endGame();
+}
 
 // add event listener on the stay button
-
-//function for event handler for the play again button
+stay.addEventListener("click", handleClickStay);
 
 // function that puts game into end of game stage
 function endGame() {
+    // display game result and play again button
     document.getElementById("play_again").style.display = "inline-block";
     document.getElementById("outcome").style.display = "block";
+    // remove ability to press other two buttons
     hit.removeEventListener("click", handleClickHit);
+    stay.removeEventListener("click", handleClickStay);
 }
